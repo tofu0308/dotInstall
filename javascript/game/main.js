f228ce1f85d2921784e9c5e90ceb9e907580e168
tofu0308 <isisaka2000@gmail.com>
2016-02-02 00:46:36 +0900

    画像を表示

--- javascript/game/main.js
+++ javascript/game/main.js
@@ -1,5 +1,23 @@
 enchant();
 
+/*
+
+Core
+-rootScene
+--Sprite(Bear)
+
+*/
+
 window.onload = function() {
-	console.log("hello world");
-}
+	var core = new Core(320,320);
+	core.preload('game/chara1.png');
+	core.onload = function(){
+		var bear = new Sprite(32, 32);
+		bear.image = core.assets['game/chara1.png'];
+		bear.x = 0;
+		bear.y = 0;
+
+		core.rootScene.addChild(bear); 
+	}
+	core.start();
+};
