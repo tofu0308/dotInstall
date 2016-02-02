b945f8fa6499c2147df39ff7229e54f7093e7169
tofu0308 <isisaka2000@gmail.com>
2016-02-03 00:13:24 +0900

    キーボードで操作

--- javascript/game/main.js
+++ javascript/game/main.js
@@ -11,7 +11,7 @@ Core
 window.onload = function() {
 	var core = new Core(320,320);
 	core.preload('game/chara1.png');
-	core.fps = 10;
+	core.fps = 5;
 
 	core.onload = function(){
 		var bear = new Sprite(32, 32);
@@ -21,12 +21,25 @@ window.onload = function() {
 
 		bear.frame = 1;
 
-		bear.addEventListener('enterframe' , function(){
-			this.x += 5;
-			this.frame = this.age % 3 + 5;
 
-			if(this.x >= 320)  this.x = 0;
+		bear.addEventListener('enterframe' , function(){
+			
+			//アニメーション
+			this.frame = this.age % 3 ;
+
+			// 十字キーで操作
+			if(core.input.left) this.x -= 5;
+			if(core.input.right) this.x += 5;
+			if(core.input.up) this.y -= 5;
+			if(core.input.down) this.y += 5;
 		});
+
+		//touch -> remove
+		bear.on('touchstart' , function(){
+			core.rootScene.removeChild(this);
+		});
+
+
 
 		core.rootScene.addChild(bear); 
 	}
