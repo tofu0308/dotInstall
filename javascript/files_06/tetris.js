b2701cc282e49903559c52256ad75263447bb48d
tofu0308 <isisaka2000@gmail.com>
2016-03-21 14:55:05 +0900

    変数追加

---
+++ javascript/files_06/tetris.js
@@ -0,0 +1,26 @@
+var COLS =10,ROWS =20;
+var board = [];
+var lose;
+var interval;
+var cuurent;
+var currentX, currentY;
+
+//block pattern
+var shapes = [
+	[1,1,1,1],
+	[1,1,1,0,
+	 1],
+	[1,1,1,0,
+	 0,0,1],
+	[1,1,0,0,
+	 1,1],
+	[1,1,0,0,
+	 0,1,1],
+	[0,1,1,0,
+	 1,1],
+	[0,1,0,0,
+	 1,1,1]
+];
+
+//block colors
+var colors = ['cyan','orange','blue','yellow','red','green','purple'];
