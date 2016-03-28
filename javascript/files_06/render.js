b44dc8e033219710422ae821c769e1d3d389db6e
tofu0308 <isisaka2000@gmail.com>
2016-03-28 15:38:15 +0900

    エラー処理

--- javascript/files_06/render.js
+++ javascript/files_06/render.js
@@ -9,13 +9,13 @@ var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
 
 function render() {
 	ctx.clearRect(0,0,W,H);
-	ctx.storokeStyle = 'black';
+	ctx.strokeStyle = 'black';
 
 	//盤面を描画
 	for (var x = 0; x < COLS; ++x) {
 		for (var y =0; y < ROWS; ++y) {
-			if(borad[x][y]){
-				ctx.fillstyle = colors[borad[y][x] - 1];
+			if ( board[ y ][ x ] ) {
+				ctx.fillStyle = colors[board[y][x] - 1];
 				drawBlock(x,y);
 			}		
 		}
@@ -25,12 +25,15 @@ function render() {
 	for ( var y = 0; y < 4; ++y ) {
 		for ( var x = 0; x < 4; ++x ) {
 			if ( current[ y ][ x ] ) {
-				ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];  // マスの種類に合わせて塗りつぶす色を設定
-				drawBlock( currentX + x, currentY + y );  // マスを描画
+				ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];
+				drawBlock( currentX + x, currentY + y );
 			}
 		}
 	}
 }
+
+// 30ミリ秒ごとに状態を描画する関数を呼び出す
+setInterval( render, 30 );
 
 // x, yの部分へマスを描画する処理
 function drawBlock( x, y ) {
