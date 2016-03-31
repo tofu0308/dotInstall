1b7776ebd948b2181942e2a6f4e2339191bfb618
tofu0308 <isisaka2000@gmail.com>
2016-03-31 15:09:15 +0900

    四角形を描画

--- javascript/07.js
+++ javascript/07.js
@@ -8,5 +8,13 @@ function draw(){
 	if(!canvas || !canvas.getContext) return false;
 	var ctx = canvas.getContext('2d');
 
-	ctx.strokeRect(10,10,50,50);
+	/*
+		引数1　支点のx座標
+		引数2　支点のy座標
+		引数3　幅
+		引数4　高さ
+	*/
+	ctx.strokeRect(10,10,50,100);
+	ctx.fillRect(10,10,100,50);
+	ctx.clearRect(20,20,20,20);
 }
