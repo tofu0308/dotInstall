92bcfd283e6fa85a7a1d093057718737349dc650
tofu0308 <isisaka2000@gmail.com>
2016-03-31 15:24:46 +0900

    線、角、塗り、グラデーションの設定

--- javascript/07.js
+++ javascript/07.js
@@ -13,8 +13,44 @@ function draw(){
 		引数2　支点のy座標
 		引数3　幅
 		引数4　高さ
-	*/
+	
 	ctx.strokeRect(10,10,50,100);
 	ctx.fillRect(10,10,100,50);
 	ctx.clearRect(20,20,20,20);
+	*/
+
+	//線の色
+	ctx.strokeStyle = "rgba(255,0,0,0.5)";
+	
+	//線の幅（太さ）
+	ctx.lineWidth = 15;
+	
+	//角の設定
+	//round　角丸
+	//bevel　面取
+	//miter 面取しない
+
+	/*
+	ctx.lineJoin = "bevel";
+	ctx.strokeRect(10,10,50,50);
+	*/
+	
+	//塗り色
+	/*
+	ctx.fillStyle = "rgba(0,255,255,0.5)"
+	ctx.fillRect(80,10,50,50);
+	*/
+
+	//線形グラデーション
+	//var g = ctx.createLinearGradient(0,0,100,100);
+
+	//円形グラデーション
+	var g = ctx.createRadialGradient(50,50,20,50,50,60);
+	
+	g.addColorStop(0.0,"red");
+	g.addColorStop(0.5,"purple");
+	g.addColorStop(1.0,"yellow");
+	ctx.fillStyle = g;
+
+	ctx.fillRect(0,0,100,100);
 }
