d2957011b8a08cf7e7607a63111570690873c3cb
tofu0308 <isisaka2000@gmail.com>
2016-05-12 16:19:34 +0900

    横の動きを追加

--- javascript/files_09/script.js
+++ javascript/files_09/script.js
@@ -1,10 +1,24 @@
 $(function(){
+	var pos1 = $('#box1').offset();
+	var pos2 = $('#box2').offset();
+	var pos3 = $('#box3').offset();
+	var pos4 = $('#box4').offset();
+
 	$(window).scroll(function(){
 		var dy = $(this).scrollTop();
 		console.log(dy);
 
-		$('#box1').css('top' , 10+dy/2);
-		$('#box2').css('top' , 10+dy/4);
-		$('#box3').css('top' , 10+dy/8);
+		$('#box1').css('top' , pos1.top + dy/2);
+		$('#box1').css('left' , pos1.left + dy/4);
+
+		$('#box2').css('top' , pos2.top + dy/4);
+		$('#box2').css('left' , pos2.left + dy/8);
+
+		$('#box3').css('top' , pos3.top + dy/6);
+		$('#box3').css('left' , pos3.left + dy/12);
+
+		$('#box4').css('top' , pos4.top + dy/8);
+		$('#box4').css('left' , pos4.left + dy/16);
+
 	})
 })
