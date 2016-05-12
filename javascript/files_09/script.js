c92e3a42d990f3c70fc4054174e351be644e78ed
tofu0308 <isisaka2000@gmail.com>
2016-05-12 16:22:18 +0900

    スクロールに応じてboxサイズ変更

--- javascript/files_09/script.js
+++ javascript/files_09/script.js
@@ -10,12 +10,16 @@ $(function(){
 
 		$('#box1').css('top' , pos1.top + dy/2);
 		$('#box1').css('left' , pos1.left + dy/4);
+		$('#box1').css('width' , 40 + dy * 0.2);
 
 		$('#box2').css('top' , pos2.top + dy/4);
 		$('#box2').css('left' , pos2.left + dy/8);
+		$('#box2').css('height' , 40 + dy * 0.2);
 
 		$('#box3').css('top' , pos3.top + dy/6);
 		$('#box3').css('left' , pos3.left + dy/12);
+		$('#box3').css('width' , 40 + dy * 0.2);
+		$('#box3').css('height' , 40 + dy * 0.2);
 
 		$('#box4').css('top' , pos4.top + dy/8);
 		$('#box4').css('left' , pos4.left + dy/16);
