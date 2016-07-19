6cf9fec1c662cc65eae9ed4caaae2f6a17b98956
tofu0308 <isisaka2000@gmail.com>
2016-07-19 16:39:30 +0900

    調整

--- javascript/files_11/script.js
+++ javascript/files_11/script.js
@@ -4,13 +4,15 @@ $(function() {
 
 		var wh =$('body').height();
 		var whPer =	wh / 100;
-		var dyPer =(dy*1.1) / whPer;
+		var dyPer =(dy*1.1) / whPer;		
 
 		var scPer =Math.round(dyPer);
 		if (scPer > 100) scPer = 100;
+		console.log(scPer);
 
 		$('.scroll .val').text(dy + ' px');
 		$('.bar').css('width' , scPer +'%');
+
 
 	});
 });
